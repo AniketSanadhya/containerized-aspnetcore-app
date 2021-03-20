485c78f5852599bbc20fbd8cf101076cb1851b8b
Aniket Sanadhya <sanadhyaaniket95@gmail.com>
2021-03-20 23:57:01 +0530

    added url interceptor

---
+++ ClientApp/src/app/home/UrlInterceptor.ts
@@ -0,0 +1,14 @@
+import { HttpRequest, HttpHandler, HttpEvent } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
+
+@Injectable()
+export class UrlInterceptor {
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const cloneReq = req.clone({ url: environment.url+'/'+ req.url });
+        return next.handle(cloneReq);
+    }
+}
+
